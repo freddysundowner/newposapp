76995c69bbab755eb460b74c562158dc559052b2
PeterKimLiz <pkironji@yahoo.com>
2022-10-19 13:40:36 +0300

    removed all comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterpos/bindings.dart';
 import 'package:flutterpos/controllers/AuthController.dart';
-import 'package:flutterpos/screens/authentication/login_page.dart';
+import 'package:flutterpos/screens/authentication/login.dart';
 import 'package:flutterpos/utils/colors.dart';
 import 'package:get/get.dart';
 void main() {
