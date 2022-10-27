de5a5745ba1de68b8a5c6ddc81a5a890e21a5daf
PeterKimLiz <githumbi3fred@email.com>
2022-10-27 09:20:58 +0300

    displayed product history

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutterpos/controllers/product_controller.dart';
 import 'package:flutterpos/controllers/shop_controller.dart';
 import 'package:flutterpos/models/product_model.dart';
+import 'package:flutterpos/widgets/pdf/bar_code_pdf.dart';
 import 'package:flutterpos/widgets/smalltext.dart';
 import 'package:get/get.dart';
 
-
-import '../../../../utils/Constants.dart';
 import '../screens/product/product_history.dart';
 import '../utils/colors.dart';
 import 'bigtext.dart';
 import 'delete_dialog.dart';
 
 Widget productCard({required context, required product, required shopId}) {
-  ShopController shopController=Get.find<ShopController>();
+  ShopController shopController = Get.find<ShopController>();
   return InkWell(
     onTap: () {
       showProductModal(context, product, shopId);
@@ -21,59 +20,62 @@ Widget productCard({required context, required product, required shopId}) {
     child: Padding(
       padding: const EdgeInsets.all(3.0),
       child: Card(
-        color:product.quantity == 0
+        color: product.quantity == 0
             ? Colors.red
-            :product.quantity == product.stockLevel? Colors.amber: AppColors.mainColor,
+            : product.quantity == product.stockLevel
+                ? Colors.amber
+                : AppColors.mainColor,
         elevation: 2,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
               child: Row(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          majorTitle(
-                              title: "${product.name}",
-                              color: Colors.white,
-                              size: 16.0),
-                          SizedBox(height: 5),
-                          minorTitle(
-                              title: "Category: ${product.category.name}",
-                              color: Colors.white),
-                          SizedBox(height: 5),
-                          minorTitle(
-                              title: "Available: ${product.quantity}",
-                              color: Colors.white),
-                          SizedBox(height: 5),
-                          minorTitle(
-                              title: "By ~ ${product.attendant?.name}",
-                              color: Colors.white),
-                        ],
-                      )
+                      majorTitle(
+                          title: "${product.name}",
+                          color: Colors.white,
+                          size: 16.0),
+                      SizedBox(height: 5),
+                      minorTitle(
+                          title: "Category: ${product.category.name}",
+                          color: Colors.white),
+                      SizedBox(height: 5),
+                      minorTitle(
+                          title: "Available: ${product.quantity}",
+                          color: Colors.white),
+                      SizedBox(height: 5),
+                      minorTitle(
+                          title: "By ~ ${product.attendant?.name}",
+                          color: Colors.white),
                     ],
-                  ),
-                  Spacer(),
-                  Row(children: [
-                    Column(
-                      children: [
-                        minorTitle(
-                            title: "BP/= ${shopController.currentShop.value?.currency}.${product.buyingPrice}",
-                            color: Colors.white),
-                        SizedBox(height: 5),
-                        minorTitle(
-                            title: "SP/=  ${shopController.currentShop.value?.currency}.${product.sellingPrice![0]}",
-                            color: Colors.white),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-
-                  ])
+                  )
                 ],
-              )),
+              ),
+              Spacer(),
+              Row(children: [
+                Column(
+                  children: [
+                    minorTitle(
+                        title:
+                            "BP/= ${shopController.currentShop.value?.currency}.${product.buyingPrice}",
+                        color: Colors.white),
+                    SizedBox(height: 5),
+                    minorTitle(
+                        title:
+                            "SP/=  ${shopController.currentShop.value?.currency}.${product.sellingPrice![0]}",
+                        color: Colors.white),
+                  ],
+                ),
+                SizedBox(width: 10),
+              ])
+            ],
+          )),
         ),
       ),
     ),
@@ -81,7 +83,7 @@ Widget productCard({required context, required product, required shopId}) {
 }
 
 showProductModal(context, ProductModel product, shopId) {
-  ProductController productController=Get.find<ProductController>();
+  ProductController productController = Get.find<ProductController>();
 
   return showModalBottomSheet<void>(
       context: context,
@@ -101,34 +103,39 @@ showProductModal(context, ProductModel product, shopId) {
                   leading: Icon(Icons.list),
                   onTap: () {
                     Get.back();
-                    Get.to(()=>ProductHistory(product:product));
-
+                    Get.to(() => ProductHistory(product: product));
                   },
                   title: Text('Product History')),
-                ListTile(
-                    leading: Icon(Icons.edit),
-                    onTap: () {
-                      Get.back();
-                    },
-                    title: Text('Edit')),
+              ListTile(
+                  leading: Icon(Icons.edit),
+                  onTap: () {
+                    Get.back();
+                  },
+                  title: Text('Edit')),
               ListTile(
                   leading: Icon(Icons.code),
                   onTap: () {
                     Get.back();
-
+                    BarcodePdf(
+                        productname: product.name,
+                        shop: Get.find<ShopController>()
+                            .currentShop
+                            .value!
+                            .name!);
                   },
                   title: Text('Generate Barcode')),
-                ListTile(
-                    leading: Icon(Icons.delete),
-                    onTap: () {
-                      Get.back();
-                      deleteDialog(
-                          context: context,
-                          onPressed: () {
-                            productController.deleteProduct(id:product.id,context:context,shopId:shopId);
-                          });
-                    },
-                    title: Text('Delete')),
+              ListTile(
+                  leading: Icon(Icons.delete),
+                  onTap: () {
+                    Get.back();
+                    deleteDialog(
+                        context: context,
+                        onPressed: () {
+                          productController.deleteProduct(
+                              id: product.id, context: context, shopId: shopId);
+                        });
+                  },
+                  title: Text('Delete')),
             ],
           ),
         );
