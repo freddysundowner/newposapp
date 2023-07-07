209f3c482a7cf817b48c1a1534d4bd7eabbf2ae4
PeterKimLiz <pkironji@yahoo.com>
2023-07-07 10:09:03 +0300

    centered the stock grid items at the center

--- lib/responsive/responsiveness.dart
+++ lib/responsive/responsiveness.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
+bool isSmallScreen(context) => MediaQuery.of(context).size.width < 768;
 
- bool isSmallScreen(context) =>
-MediaQuery.of(context).size.width < 768;
+bool isDesktop(BuildContext context) =>
+    MediaQuery.of(context).size.width >= 768;
 
- bool isDesktop(BuildContext context) =>
-MediaQuery.of(context).size.width >= 768;
 class ResponsiveWidget extends StatelessWidget {
   final Widget largeScreen;
 
@@ -17,16 +16,15 @@ class ResponsiveWidget extends StatelessWidget {
     required this.smallScreen,
   }) : super(key: key);
 
-
   static bool isTabletScreen(BuildContext context) =>
       MediaQuery.of(context).size.width < 1200 &&
       MediaQuery.of(context).size.width >= 768;
-
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constrains) {
       double _width = constrains.maxWidth;
+      print(_width);
       if (_width <= 768) {
         return smallScreen;
       } else {
